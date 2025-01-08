84ab6943ade8788e600e749e4ea0602799203bc5
Michael Corrales <corralesmendezm@gmail.com>
2025-01-08 12:51:35 -0500

    Inicio del proyecto JAVABank ATM

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; // Genera una excepción
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir por cero!");
+        }
+    }
+}
