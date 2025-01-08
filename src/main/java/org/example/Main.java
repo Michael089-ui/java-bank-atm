039766e9d5ae90210e362fcb227243755431d421
Michael Estiven Corrales Mendez <mcorrales@poligran.edu.co>
2025-01-08 14:50:45 -0500

    Actualizacion para subir repo a cuenta de git hub

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,3 +5,9 @@ public class Main {
         System.out.println("Welcome to JavaBank ATM! Version control with Git.");
     }
 }
+
+//# Añadir el repositorio remoto
+//git remote add origin https://github.com/tu-usuario/java-bank-atm.git
+//
+//# Empujar los cambios iniciales
+//git push -u origin main
